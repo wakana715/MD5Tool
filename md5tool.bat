0eef72adfa5198b0b36b6b4efa2f6dab042489c6
wakana715 <52075149+wakana715@users.noreply.github.com>
2025-02-18 14:17:56 +0900

    Delete md5tool.bat

--- md5tool.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem" /v "LongPathsEnabled" /t "REG_DWORD" /d "1" /f
-powershell -NoProfile -ExecutionPolicy Unrestricted .\md5tool.ps1 %1
